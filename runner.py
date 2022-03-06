69dba4565fa1d6cebef73abbbf0e94f7993f2768
Author Name <email@address.com>
2022-03-06 15:52:45 +0200

    python way

--- runner.py
+++ runner.py
@@ -9,15 +9,12 @@ from stem import Signal
 from stem.control import Controller
 import socks, socket
 
-msg = "Vashy voiny ubivaiut mirnoe naselenie v Ukraine"
-
-def call_me(num):
+def call_me(num, msg):
 	try:
-		controller.signal(Signal.NEWNYM)
-		time.sleep(controller.get_newnym_wait())
 		current_ip = requests.get(url='http://icanhazip.com/')
 		print(f"Sending to {num} from IP: {current_ip.text}")
-
+  
+		session = requests.Session()
 		session.head('http://ossinfo.ru/otpravit-sms-na-mts.html')
 		response = session.post(
 			url='http://ossinfo.ru/functions/custom.php',
@@ -37,26 +34,34 @@ def call_me(num):
 		print(f"Facing error:\n{e}\nDon't bother unless it's persistent")
 
 
-if len(sys.argv) >= 2:
-	if len(sys.argv[1]) >= 3:
-		msg = sys.argv[1]
+def main():
+	msg = "Vashy voiny ubivaiut mirnoe naselenie v Ukraine"
 
-print(f"Going to spam the following message:\n{msg}")
+	if len(sys.argv) >= 2:
+		if len(sys.argv[1]) >= 3:
+			msg = sys.argv[1]
 
-with Controller.from_port(port=9051) as controller:
-	controller.authenticate(password='')
-	socks.setdefaultproxy(proxy_type=socks.PROXY_TYPE_SOCKS5, addr="127.0.0.1", port=9050)
-	socket.socket = socks.socksocket
-	session = requests.Session()
+	print(f"Going to spam the following message:\n{msg}")
 
-	while True:
-		with open('phones_pikabu.txt', 'r') as file:
-			lines = file.readlines() 
-			random_line = random.choice(lines) if lines else None
-			num = re.sub('\D', '', random_line)
-			if len(num) == 11:
-				call_me(num)
-			elif len(num) % 11 == 0:
-				for n in textwrap.wrap(num, 11):
-					call_me(num)
+	with Controller.from_port(port=9051) as controller:
+		controller.authenticate(password='')
+		socks.setdefaultproxy(proxy_type=socks.PROXY_TYPE_SOCKS5, addr="127.0.0.1", port=9050)
+		socket.socket = socks.socksocket
 
+		while True:
+			with open('phones_pikabu.txt', 'r') as file:
+				lines = file.readlines() 
+				random_line = random.choice(lines) if lines else None
+				num = re.sub('\D', '', random_line)
+				if len(num) == 11:
+					call_me(num, msg)
+				elif len(num) % 11 == 0:
+					for n in textwrap.wrap(num, 11):
+						call_me(num, msg)
+      # reset Tor node
+			controller.signal(Signal.NEWNYM)
+			time.sleep(controller.get_newnym_wait())
+
+
+if __name__ == "__main__":
+  main()
